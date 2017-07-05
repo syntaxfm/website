c9b0baba299c92056bf4647fb0a7fc3c2cd6d37b
Adrian Daras <getpingui@gmail.com>
2017-07-05 15:16:26 +0300

    Make config immutable

--- next.config.js
+++ next.config.js
@@ -2,35 +2,44 @@ const path = require('path');
 const glob = require('glob');
 
 module.exports = {
-  webpack: (config, { dev }) => {
-    config.module.rules.push(
-      {
-        test: /\.(css|styl)/,
-        loader: 'emit-file-loader',
-        options: {
-          name: 'dist/[path][name].[ext]'
-        }
-      }
-    ,
-      {
-        test: /\.css$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader']
-      }
-    ,
-      {
-        test: /\.styl$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader',
-          { loader: 'stylus-loader',
-            options: {
-              includePaths: ['styles', 'node_modules']
-                .map((d) => path.join(__dirname, d))
-                .map((g) => glob.sync(g))
-                .reduce((a, c) => a.concat(c), [])
-            }
+  webpack: (config) => Object.assign({}, config, {
+    module: Object.assign({}, config.module, {
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.(css|styl)/,
+          loader: 'emit-file-loader',
+          options: {
+            name: 'dist/[path][name].[ext]'
           }
-        ]
-      }
-    )
-    return config
-  }
-}
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'babel-loader',
+            'raw-loader',
+            'postcss-loader',
+          ]
+        },
+        {
+          test: /\.styl$/,
+          use: [
+            'babel-loader',
+            'raw-loader',
+            'postcss-loader',
+            {
+              loader: 'stylus-loader',
+              options: {
+                includePaths:
+                  ['styles', 'node_modules']
+                    .map((d) => path.join(__dirname, d))
+                    .map((g) => glob.sync(g))
+                    .reduce((a, c) => a.concat(c), []),
+              },
+            },
+          ],
+        },
+      ],
+    }),
+  }),
+};
