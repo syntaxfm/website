1db997e384ba96b42b7f5a11f1bafd963c586967
Jaco Ahmad <jaco.ahmad@gmail.com>
2018-03-25 14:17:14 +0700

    Refactor empty JSX to self closed JSX

--- components/ShowList.js
+++ components/ShowList.js
@@ -4,5 +4,5 @@ import Show from './Show';
 export default ({ shows, currentPlaying, currentShow, setCurrentPlaying }) =>
   <div className="showList">
     {shows.map(show => <Show setCurrentPlaying={setCurrentPlaying} currentPlaying={currentPlaying} currentShow={currentShow} key={show.number} show={show} />)}
-    <div className="show show--dummy"></div>
+    <div className="show show--dummy"/>
   </div>
