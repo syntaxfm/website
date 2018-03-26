38e19a9041f8df057ed165c78c9d86e915dc6ff7
Jaco Ahmad <jaco.ahmad@gmail.com>
2018-03-26 22:44:09 +0700

    Update bars.js

--- components/bars.js
+++ components/bars.js
@@ -1,11 +1,11 @@
 export default () => (
   <div className="bars bars--paused">
-      <div className="bar"/>
-      <div className="bar"/>
-      <div className="bar"/>
-      <div className="bar"/>
-      <div className="bar"/>
-      <div className="bar"/>
-      <div className="bar"/>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
   </div>
 )
