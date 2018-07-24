402c28ca13bf2f6df300a99c4b2b59de4eac80dd
Logan Garland <lgarland@marana.com>
2018-07-24 11:05:37 -0700

    - refactored ShowNotes to class component - added auto scroll functionality to bring show notes in to view

--- components/ShowNotes.js
+++ components/ShowNotes.js
@@ -1,13 +1,49 @@
-import React from 'react'
-import Show from './Show';
+import React from "react";
+import Show from "./Show";
 
-export default ({ show, setCurrentPlaying }) =>
-  <div className="showNotes">
-    <p className="show__date">{show.displayDate}</p>
-    <h2>{show.title}</h2>
-    <button className="button" onClick={() => setCurrentPlaying(show.displayNumber)}><span className="icon">🎵</span> Play Episode {show.displayNumber}</button>
-    <a className="button" download href={show.url}><span className="icon">👇</span> Download Show</a>
-    <a className="button" href={`https://github.com/wesbos/Syntax/edit/master/${show.notesFile}`} target='_blank'><span className="icon">✏️</span> Edit Show Notes</a>
-    <div dangerouslySetInnerHTML={{ __html: show.html }}></div>
-  </div>
+export default class ShowNotes extends React.Component {
+  componentDidUpdate(prevProps) {
+    if (prevProps.show !== this.props.show) {
+      const playerHeight = document.querySelector(".player").clientHeight;
+      const scrollTop = this.container.offsetTop - playerHeight;
 
+      if (window.scrollY > scrollTop || scrollTop > window.screen.height) {
+        window.scroll({
+          top: scrollTop,
+          left: 0,
+          behavior: "smooth"
+        });
+      }
+    }
+  }
+
+  render() {
+    const { show, setCurrentPlaying } = this.props;
+
+    return (
+      <div className="showNotes" ref={el => (this.container = el)}>
+        <p className="show__date">{show.displayDate}</p>
+        <h2>{show.title}</h2>
+        <button
+          className="button"
+          onClick={() => setCurrentPlaying(show.displayNumber)}
+        >
+          <span className="icon">🎵</span> Play Episode {show.displayNumber}
+        </button>
+        <a className="button" download href={show.url}>
+          <span className="icon">👇</span> Download Show
+        </a>
+        <a
+          className="button"
+          href={`https://github.com/wesbos/Syntax/edit/master/${
+            show.notesFile
+          }`}
+          target="_blank"
+        >
+          <span className="icon">✏️</span> Edit Show Notes
+        </a>
+        <div dangerouslySetInnerHTML={{ __html: show.html }} />
+      </div>
+    );
+  }
+}
