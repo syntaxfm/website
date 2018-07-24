d6c401f9679a3261c2ec1f8e2fa2d8970469c28c
Logan Garland <lgarland@marana.com>
2018-07-24 11:37:32 -0700

    fixed formatting

--- components/ShowNotes.js
+++ components/ShowNotes.js
@@ -24,25 +24,10 @@ export default class ShowNotes extends React.Component {
       <div className="showNotes" ref={el => (this.container = el)}>
         <p className="show__date">{show.displayDate}</p>
         <h2>{show.title}</h2>
-        <button
-          className="button"
-          onClick={() => setCurrentPlaying(show.displayNumber)}
-        >
-          <span className="icon">🎵</span> Play Episode {show.displayNumber}
-        </button>
-        <a className="button" download href={show.url}>
-          <span className="icon">👇</span> Download Show
-        </a>
-        <a
-          className="button"
-          href={`https://github.com/wesbos/Syntax/edit/master/${
-            show.notesFile
-          }`}
-          target="_blank"
-        >
-          <span className="icon">✏️</span> Edit Show Notes
-        </a>
-        <div dangerouslySetInnerHTML={{ __html: show.html }} />
+        <button className="button" onClick={() => setCurrentPlaying(show.displayNumber)}><span className="icon">🎵</span> Play Episode {show.displayNumber}</button>
+        <a className="button" download href={show.url}><span className="icon">👇</span> Download Show</a>
+        <a className="button" href={`https://github.com/wesbos/Syntax/edit/master/${show.notesFile}`} target='_blank'><span className="icon">✏️</span> Edit Show Notes</a>
+        <div dangerouslySetInnerHTML={{ __html: show.html }}></div>
       </div>
     );
   }
