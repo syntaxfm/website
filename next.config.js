05bc3301b094cd814fc53006cd10f20d4fc66651
Logan Garland <lgarland@marana.com>
2018-07-24 11:30:05 -0700

    added smoothscroll polyfill

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const glob = require('glob');
 
 module.exports = {
   webpack: (config, { dev }) => {
+    const originalEntry = config.entry
+    config.entry = async () => {
+      const entries = await originalEntry()
+
+      if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
+        entries['main.js'].unshift('./client/polyfills.js')
+      }
+
+      return entries
+    }
+
     config.module.rules.push(
       {
         test: /\.(css|styl)/,
