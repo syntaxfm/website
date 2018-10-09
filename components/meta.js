506a024a35138d1471bfa5f658cdf862ce03d0eb
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-10-09 08:56:14 +0300

    Closes #178: Add meta description tag

--- components/meta.js
+++ components/meta.js
@@ -8,6 +8,10 @@ export default ({ show, baseURL }) => (
       <html lang="en"/>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <meta
+        name="description"
+        content="Full Stack Developers Wes Bos and Scott Tolinski dive deep into web development topics, explaining how they work and talking about their own experiences. They cover from JavaScript frameworks like React, to the latest advancements in CSS to simplifying web tooling."
+      />
       <meta property="og:audio" content={show.url} />
       <meta property="og:audio:secure_url" content={show.url} />
       <meta property="og:audio:type" content="audio/mp3" />
