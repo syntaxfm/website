6197890edcc1a594b5d623bbe6be948efe09415c
shortnd <ococncol@gmail.com>
2018-10-08 23:41:48 -0400

    added high contrast mode button to help eliminate color contrast issues

--- components/Page.js
+++ components/Page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Meta from '../components/meta';
+import HighContrast from '../components/highcontrast';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import stylesheet from '../styles/style.styl';
@@ -9,6 +10,8 @@ export default class Page extends React.Component {
     return (
       <div className="page">
         <a href="#main" className="skip-link">Skip to content</a>
+        {/* <a href="#" id="highcontrast">High Contrast Mode</a> */}
+        <HighContrast />
         <Header />
         <style dangerouslySetInnerHTML={{ __html: stylesheet.replace(/\n/g, '') }} />
         {this.props.children}
---
+++ components/highcontrast.js
@@ -0,0 +1,25 @@
+export default class highContrast extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { isActive: false};
+    this.highContrastMode = this.highContrastMode.bind(this);
+  }
+
+  highContrastMode(e) {
+    e.preventDefault();
+    this.setState(state => ({
+      isActive: !state.isActive
+    }));
+    let html = document.querySelector('html');
+    html.classList.toggle('highcontrast');
+    console.log(html.classList);
+  }
+
+  render() {
+    return (
+      <a href="#" id="highcontrast" onClick={this.highContrastMode}>
+        {this.state.isActive ? 'High Contrast Mode Off': 'High Contrast Mode'}
+      </a>
+    );
+  }
+}
