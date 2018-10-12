e0d8f9ba72d295d6f4cb2db59f94ecd1958761fa
Roberto Sousa <>
2018-10-12 11:23:08 +0100

    render the books page

---
+++ pages/sickbooks.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Page from '../components/Page';
+import axios from 'axios';
+
+export default class SponsorPage extends React.Component {
+  componentDidCatch = e => {
+    console.log('ERRORRRRR');
+    console.log(e);
+  };
+
+  static async getInitialProps({ req }) {
+    const protocol = req && req.headers.host.indexOf('syntax.fm') > -1 ? 'https' : req ? req.protocol : '';
+    const baseURL = req ? `${protocol}://${req.headers.host}` : window.location.origin;
+    const { data: books } = await axios.get(`${baseURL}/api/books`);
+    return { books };
+  }
+
+  render() {
+    const { books = {} } = this.props;
+    return (
+      <Page>
+        <div className="wrapper wrapper--text center">
+          <h1>Sick Books</h1>
+          <div dangerouslySetInnerHTML={{ __html: books.html }}></div>
+        </div>
+      </Page>
+    );
+  }
+}
