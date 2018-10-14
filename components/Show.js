2097436bb65442f4c5d5f9425c3ff3c403ec7360
Simey de Klerk <simeydeklerk@outlook.com>
2018-10-14 14:38:34 +0200

    convert Show.js to stateless functional component

--- components/Show.js
+++ components/Show.js
@@ -5,27 +5,30 @@ import Router from 'next/router'
 import Bars from './bars';
 import { FaPlay } from "react-icons/fa";
 
-export default class Show extends React.Component {
-  changeURL = (e, show) => {
+const Show = ({ show, currentPlaying, currentShow, setCurrentPlaying }) => {
+    
+  const [isPlaying, isDisplayed] = [currentPlaying, currentShow].map(currentX => show.displayNumber === currentX)
+  const showClassName = ['show', isPlaying ? 'show--playing' : '', isDisplayed ? 'show--active' : ''].join(' ')
+
+  const changeURL = (e, show) => {
     e.preventDefault();
     const href = e.currentTarget.href;
     Router.push(`/?number=${show.displayNumber}`, href, { shallow: true })
   }
+  const PlayButton = () => <button onClick={() => setCurrentPlaying(show.displayNumber)} className="show__play" title="play button" ><FaPlay/></button>
 
-  render() {
-    const { show, currentPlaying, currentShow, setCurrentPlaying } = this.props;
-    return (
-      <div className={`show ${currentPlaying === show.displayNumber ? 'show--playing' : '' } ${currentShow === show.displayNumber ? 'show--active' : '' }
-      `}>
-        <a className="show__link" href={`/show/${show.displayNumber}/${slug(show.title)}`} onClick={(e) => this.changeURL(e, show)}>
-          <p className="show__displayNumber">Episode {show.displayNumber}</p>
-          <h3 className="show__title">{show.title}</h3>
-        </a>
+  return (
+    <div className={showClassName}>
+      <a className="show__link" href={`/show/${show.displayNumber}/${slug(show.title)}`} onClick={(e) => changeURL(e, show)}>
+        <p className="show__displayNumber">Episode {show.displayNumber}</p>
+        <h3 className="show__title">{show.title}</h3>
+      </a>
 
-        <div className="show__playcontrols">
-          {currentPlaying === show.displayNumber ? <Bars/ > : <button onClick={() => setCurrentPlaying(show.displayNumber)} className="show__play" title="play button" ><FaPlay/></button> }
-        </div>
+      <div className="show__playcontrols">
+        {isPlaying ? <Bars/ > :  <PlayButton />}
       </div>
-    )
-  }
-}
+    </div>
+  )
+};
+
+export default Show;
