71afbdf566fb3a4952152ff36dc1ac1986cc7643
papascott <papascott@gmail.com>
2018-10-25 11:14:54 +0200

    Fixes #218 create staticPage prop in meta.js, output tags conditionally on show prop

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import ShowNotes from '../components/ShowNotes';
 import Player from '../components/Player';
 import Meta from '../components/meta';
 import Page from '../components/Page';
+import getBaseURL from '../lib/getBaseURL';
 
 export default withRouter(
   class IndexPage extends React.Component {
@@ -28,17 +29,7 @@ export default withRouter(
     }
 
     static async getInitialProps({ req }) {
-      /*eslint-disable */
-      const protocol =
-        req && req.headers.host.indexOf('syntax.fm') > -1
-          ? 'https'
-          : req
-            ? req.protocol
-            : '';
-      /* eslint-enable */
-      const baseURL = req
-        ? `${protocol}://${req.headers.host}`
-        : window.location.origin;
+      const baseURL = getBaseURL(req);
       const { data: shows } = await axios.get(`${baseURL}/api/shows`);
       return { shows, baseURL };
     }
