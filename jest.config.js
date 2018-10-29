b328d53f2869a1aff3a1f3d549071898fd4b2332
Kevin McKernan <kevin@mckernan.in>
2018-10-28 18:20:32 -0600

    add jest setup & config

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  collectCoverageFrom: [
+    "components/**/*.{js,jsx}",
+    "lib/**/*.{js,jsx}",
+    "pages/**/*.{js,jsx}"
+  ],
+  testMatch: [
+    "<rootDir>/app/**/__tests__/**/*.{js,jsx}",
+    "<rootDir>/app/**/?(*.)(spec|test).{js,jsx}",
+    "<rootDir>/lib/**/__tests__/**/*.{js,jsx}",
+    "<rootDir>/lib/**/?(*.)(spec|test).{js,jsx}",
+    "<rootDir>/pages/**/__tests__/**/*.{js,jsx}",
+    "<rootDir>/pages/**/?(*.)(spec|test).{js,jsx}"
+  ],
+  testEnvironment: "node",
+  testURL: "http://localhost",
+  transform: {
+    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.css$": "<rootDir>/lib/cssTransform.js",
+    "^(?!.*\\.(js|jsx|mjs|css|json)$)": "<rootDir>/lib/fileTransform.js"
+  },
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"],
+  moduleFileExtensions: ["js", "json", "jsx", "node"],
+  moduleDirectories: ["node_modules", "app", "internals"]
+};
