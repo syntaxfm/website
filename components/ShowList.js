784ec6a4d4452b797875905542d803a98cf780bc
Kevin Østerkilde <kevin@oesterkilde.dk>
2018-12-21 17:20:14 +0100

    Cleanup

--- components/ShowList.js
+++ components/ShowList.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import getConfig from 'next/config'
 import { Configure,   InstantSearch,   SearchBox, InfiniteHits, PoweredBy } from 'react-instantsearch-dom';
-import { FaMicrophone } from "react-icons/fa";
+import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
 import Show from '../components/Show';
 import ShowNotes from '../components/ShowNotes';
 
-const ShowList = ({ show, currentPlaying, currentShow, setCurrentPlaying }) => {
-  const { ALGOLIA_APP_ID, ALGOLIA_API_KEY, ALGOLIA_INDEX_NAME } = getConfig().publicRuntimeConfig;
+export default class ShowList extends Component {
+  static propTypes = {
+    show: PropTypes.any.isRequired,
+    currentPlaying: PropTypes.string.isRequired,
+    currentShow: PropTypes.string.isRequired,
+    setCurrentPlaying: PropTypes.func.isRequired,
+  };
 
-  const handleSpeech = event => {
-    event.preventDefault();
+  constructor(props) {
+    super(props)
+    this.state = {
+      supportsSpeech: false,
+      isTalking: false,
+    }
+  };
 
+  componentDidMount = () => {
     window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
     if ('SpeechRecognition' in window) {
-      const recognition = new webkitSpeechRecognition();
+      console.log('Speech recognition Chrome API supported');
+      this.setState({
+        supportsSpeech: true,
+      });
+    }
+  };
 
+  handleSpeech = event => {
+    event.preventDefault();
+    this.setState({
+      isTalking: !this.state.isTalking,
+    })
+
+    if (this.state.supportsSpeech) {
+      const recognition = new webkitSpeechRecognition();
       recognition.interimResults = true;
 
       recognition.onresult = e => {
         let query = e.results[0][0].transcript;
-        this.search.refine(query)
+        this.search.refine(query);
       }
 
-      if (event.target.classList.contains('ais-microphone-on')) {
-        event.target.classList.remove('ais-microphone-on');
+      if (this.state.isTalking) {
         recognition.stop();
       } else {
-        event.target.classList.add('ais-microphone-on');
         recognition.start();
       }
     } else {
@@ -35,19 +57,25 @@ const ShowList = ({ show, currentPlaying, currentShow, setCurrentPlaying }) => {
     }
   }
 
+  render() {
+  const { show, currentPlaying, currentShow, setCurrentPlaying } = this.props;
+  const { ALGOLIA_APP_ID, ALGOLIA_API_KEY, ALGOLIA_INDEX_NAME } = getConfig().publicRuntimeConfig;
+  const speechClasses = this.state.isTalking ? 'ais-microphone-on' : '';
+
   return (
-    <InstantSearch
-      appId={ALGOLIA_APP_ID}
-      apiKey={ALGOLIA_API_KEY}
-      indexName={ALGOLIA_INDEX_NAME}
-    >
+    <InstantSearch appId={ALGOLIA_APP_ID} apiKey={ALGOLIA_API_KEY} indexName={ALGOLIA_INDEX_NAME}>
       <Configure hitsPerPage={25} />
       <div className="ais-search">
         <SearchBox translations={{ placeholder: 'Search for episodes...' }} ref={node => this.search = node} />
         <PoweredBy />
-        <button className="ais-microphone" onClick={handleSpeech}>
-          <FaMicrophone />
-        </button>
+        {this.state.supportsSpeech && this.state.isTalking
+          ? <button className={`ais-microphone ${speechClasses}`} onClick={this.handleSpeech}>
+              <FaMicrophoneSlash />
+            </button>
+          : <button className={`ais-microphone ${speechClasses}`} onClick={this.handleSpeech}>
+              <FaMicrophone />
+            </button>
+        }
       </div>
       <div className="ais-instantSearch">
         <InfiniteHits
@@ -64,13 +92,5 @@ const ShowList = ({ show, currentPlaying, currentShow, setCurrentPlaying }) => {
       </div>
     </InstantSearch>
   );
+  }
 };
-
-ShowList.propTypes = {
-  show: PropTypes.any.isRequired,
-  currentPlaying: PropTypes.string.isRequired,
-  currentShow: PropTypes.string.isRequired,
-  setCurrentPlaying: PropTypes.func.isRequired,
-};
-
-export default ShowList;
