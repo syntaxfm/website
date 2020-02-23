df5ce94b8394e65c21084d75438e78cf763e8921
Brian LeRoux <b@brian.io>
2020-02-23 10:41:15 -0800

    fix sick picks say that three times fast

--- src/views/pages/sickpicks.tsx
+++ src/views/pages/sickpicks.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Page from '../components/Page';
 
 export default function SickPicksPage(props) {
+  const [shows, setShows] = useState([]);
+  if (typeof fetch != 'undefined') {
+    useEffect(() => {
+      !async function() {
+        let result = await (await fetch('/api/sickpicks')).json()
+        setShows(result)
+      }()
+    }, []);
+  }
   return (
     <Page>
       <div className="wrapper wrapper--text">
-        {props.picks.map(sickPick => (
+        {shows.map(sickPick => (
           <div
             key={sickPick.id}
             dangerouslySetInnerHTML={{ __html: sickPick.html }} //eslint-disable-line
