f26973d9a48ba8a1939e12061181c2c8e6b8ccb4
Joe Haddad <timer150@gmail.com>
2020-06-24 11:31:37 -0400

    Re-add API endpoints

---
+++ pages/api/shows/latest.js
@@ -0,0 +1,21 @@
+import Cors from 'cors';
+import { getShows } from '../../../lib/getShows';
+import initMiddleware from '../../../lib/initMiddleware';
+
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET', 'OPTIONS'],
+  })
+);
+
+export default async function latest(req, res) {
+  await cors(req, res);
+
+  const shows = await getShows();
+  const show = shows[0];
+  if (show) {
+    res.json(show);
+    return;
+  }
+  res.status(404).json({ message: 'Sorry not found' });
+}
