b0115401332aa1c1d9a3bfef40b674ba372b5a9f
Simey de Klerk <13088589+simeydk@users.noreply.github.com>
2022-03-25 12:11:12 +0000

    move Head from _document.js to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,32 @@
 import PropTypes from 'prop-types';
+import Head from 'next/head';
+import packageInfo from '../package.json';
+import stylesheet from '../styles/style.styl';
+
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <>
+    <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content={packageInfo.description} />
+          <meta name="theme-color" content="#F1C15D" />
+          <meta charSet="utf-8" />
+          <meta property="og:description" content={packageInfo.description} />
+          <meta
+            property="og:image"
+            content="https://syntax.fm/static/syntax-banner.png"
+          />
+          <link rel="icon" href="https://syntax.fm/static/favicon.png" />
+          <style
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: stylesheet.replace(/\n/g, '') }}
+          />
+        </Head>
+        <Component {...pageProps} />;
+    </>
 }
 
 MyApp.propTypes = {
-  Component: PropTypes.node,
-  pageProps: PropTypes.any,
+    Component: PropTypes.node,
+    pageProps: PropTypes.any,
 };
--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import packageInfo from '../package.json';
-import stylesheet from '../styles/style.styl';
 
 class SyntaxDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="description" content={packageInfo.description} />
-          <meta name="theme-color" content="#F1C15D" />
-          <meta charSet="utf-8" />
-          <meta property="og:description" content={packageInfo.description} />
-          <meta
-            property="og:image"
-            content="https://syntax.fm/static/syntax-banner.png"
-          />
-          <link rel="icon" href="https://syntax.fm/static/favicon.png" />
-          <style
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html: stylesheet.replace(/\n/g, '') }}
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
