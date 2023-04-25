37e2d6f0929cc660f6938880b8f62528e825fed3
Scott Tolinski <scott@leveluptuts.com>
2023-04-25 13:31:09 -0600

    creates header and global stylesheet

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Header from './Header.svelte';
+	import './style.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		background-color: var(--sheet-bg);
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	$: console.log('page', $page);
+</script>
+
+{#if $page.route.id === '/'}
+	<h1>Syntax</h1>
+{:else}
+	<h2>Syntax</h2>
+{/if}
