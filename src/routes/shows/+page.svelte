b1fbf6a48973552bfdb3016417a5e51647314a1b
Scott Tolinski <scott@leveluptuts.com>
2023-05-23 17:01:19 -0600

    adds page transitions to a few more things

--- src/routes/shows/+page.svelte
+++ src/routes/shows/+page.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
+	import { format_show_type } from '$utilities/format_show_type';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 	$: ({ shows } = data);
 </script>
 
-{#each shows as show (show.id)}
-	<a href="shows/{show.number}/{show.slug}">
-		{show.title}
-	</a>
-{/each}
+<section>
+	{#each shows as show (show.id)}
+		<article>
+			<a href="shows/{show.number}/{show.slug}">
+				<p>
+					Episode {show.number} | {format_show_type(show.date)}
+				</p>
+				<h3 style:--transition-name="show-title-{show.number}">{show.title}</h3>
+			</a>
+		</article>
+	{/each}
+</section>
+
+<style>
+	h3 {
+		view-transition-name: var(--transition-name);
+	}
+</style>
