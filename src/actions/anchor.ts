c9f124d8fcde060a0ff553cc4b7bcfee81012e50
Scott Tolinski <scott@leveluptuts.com>
2023-06-22 19:20:11 -0600

    adds resize observer and window event listener to anchor action

--- src/actions/anchor.ts
+++ src/actions/anchor.ts
@@ -1,11 +1,24 @@
 import type { Action } from 'svelte/action';
 
 export const anchor: Action<HTMLElement, string> = (node: HTMLElement, id: string) => {
-	const anchor_position = node.getBoundingClientRect();
-	const target = document.getElementById(id);
-	if (target) {
-		target.style.inset = 'unset';
-		target.style.left = anchor_position.left + 'px';
-		target.style.top = anchor_position.bottom + 'px';
+	function updatePosition() {
+		const anchor_position = node.getBoundingClientRect();
+		const target = document.getElementById(id);
+		if (target) {
+			target.style.inset = 'unset';
+			target.style.left = anchor_position.left + 'px';
+			target.style.top = anchor_position.bottom + 'px';
+		}
 	}
+
+	// Use the ResizeObserver to watch for changes in size of 'node'
+	const resizeObserver = new ResizeObserver(updatePosition);
+	resizeObserver.observe(node);
+	window.addEventListener('resize', updatePosition);
+	return {
+		destroy() {
+			resizeObserver.disconnect();
+			window.removeEventListener('resize', updatePosition);
+		}
+	};
 };
