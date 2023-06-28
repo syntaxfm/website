b8c16e85ef9cca49631b2d1fd4049addbea38895
Scott Tolinski <scott@leveluptuts.com>
2023-06-28 14:34:31 -0600

    adds podcasts link icons and colors

--- src/lib/PodcastHero.svelte
+++ src/lib/PodcastHero.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-	import { PODCAST_LINKS } from '$const';
 	import HostsAndGuests from '../routes/shows/[show_number]/[slug]/HostsAndGuests.svelte';
+	import PodcastLinks from './PodcastLinks.svelte';
 </script>
 
 <section>
 	<h3>A Tasty Treats Podcast for Web Developers</h3>
 	<HostsAndGuests />
-	<div>
-		{#each PODCAST_LINKS as { text, href }}
-			<a {href} class="button">{text}</a>
-		{/each}
-	</div>
+	<PodcastLinks />
 </section>
