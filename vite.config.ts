2cd0494bcc3a6421dce9e875b12b18221fd067a3
Scott Tolinski <scott@leveluptuts.com>
2023-06-30 12:32:14 -0600

    chore(package.json): add svelte-preprocess package as a dev dependency to support preprocessing Svelte components chore(pnpm-lock.yaml): update svelte-preprocess version in the lock file to match the added dev dependency style(HostsAndGuests.svelte): change the gap value in the .featuring class from 20% to 20px for better visual spacing chore(svelte.config.js): update the preprocess configuration to use svelte-preprocess and add custom media queries and postcss plugins for better styling capabilities chore(vite.config.ts): update the css preprocessorOptions to include custom media queries and postcss plugins for better styling capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
@@ -6,5 +6,22 @@ export default defineConfig({
 	plugins: [sentrySvelteKit(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	css: {
+		preprocessorOptions: {
+			postcss: {
+				additionalData: `
+				@custom-media --below_small (width < 400px);
+				@custom-media --below_med (width < 700px);
+				@custom-media --below_large (width < 900px);
+				@custom-media --below_xlarge (width < 1200px);
+
+				@custom-media --above_small (width > 400px);
+				@custom-media --above_med (width > 700px);
+				@custom-media --above_large (width > 900px);
+				@custom-media --above_xlarge (width > 1200px);
+				`
+			}
+		}
 	}
 });
