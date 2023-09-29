9748389c35828b7930c3dcc6fdba8728e1957993
Ben Vinegar <ben@sentry.io>
2023-09-29 18:18:19 -0400

    Add static pages

---
+++ src/routes/pages/[page]/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+import remarkGfm from 'remark-gfm';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+
+var input = `
+## Lol
+*whatsup*
+`;
+
+var processor = unified()
+  .use(remarkParse)
+  .use(remarkGfm)
+  .use(remarkRehype)
+  .use(rehypeStringify);
+
+var content = processor.processSync(input).value;
+</script>
+
+<div>
+{@html content}
+</div>
