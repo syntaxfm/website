f04d6280db68d96e67135b29130c9fb148aaf306
Ratul Maharaj <ratulmaharaj@gmail.com>
2023-10-17 22:43:25 +0200

    Rename and fix redirect

--- src/routes/(site)/[show_number]/+page.server.ts
+++ src/routes/(site)/[show_number]/+page.server.ts
@@ -6,8 +6,8 @@ export const load: PageServerLoad = async function ({ params, locals, setHeaders
 		'cache-control': 'max-age=240'
 	});
 
-	const { number } = params;
-	const show = await locals.prisma.show.findFirst({ where: { number: parseInt(number) } });
-	if (show) throw redirect(302, `/shows/${number}/${show.slug}`);
-	throw redirect(302, `/shows}`);
+	const { show_number } = params;
+	const show = await locals.prisma.show.findFirst({ where: { number: parseInt(show_number) } });
+	if (show) throw redirect(302, `/shows/${show_number}/${show.slug}`);
+	throw redirect(302, `/shows`);
 };
