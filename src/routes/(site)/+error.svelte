33ef28ae35692d22d7edc322c21c014b5be4dedd
Wes Bos <wesbos@gmail.com>
2023-10-23 13:17:52 -0400

    disallow viewing of future shows if not an admin

--- src/routes/(site)/+error.svelte
+++ src/routes/(site)/+error.svelte
@@ -1,4 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
 <div class="error">
 	<h1>Oopsie-daisy</h1>
-	<p>Something went wrong. Don't worry, we use Sentry!</p>
+	{#if $page.error.message}
+		<p>{$page.error.message}</p>
+	{:else}
+		<p>Something went wrong. Don't worry, we use Sentry!</p>
+	{/if}
 </div>
