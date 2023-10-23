542de08c26360e1a3b5d580af88358367f43da61
Wes Bos <wesbos@gmail.com>
2023-10-23 15:06:20 -0400

    fixes close button not being clickable. #1247

--- src/params/ThemeMaker.svelte
+++ src/params/ThemeMaker.svelte
@@ -40,9 +40,7 @@
 		use:clickOutside
 		on:click-outside={theme_maker.close}
 	>
-		<button class="close" on:click={theme_maker.close}>×</button>
-
-		<h4>👩‍🎨</h4>
+		<h4>👩‍🎨 <button class="close" on:click={theme_maker.close}>×</button></h4>
 
 		<div class="theme-maker-buttons">
 			{#each theme_names as theme_name}
