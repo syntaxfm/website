1fdc51d5d8553b4e21bb4be32c5f68d0ea0f0c53
Wes Bos <wesbos@gmail.com>
2023-10-27 17:10:10 -0400

    reinstate vercel stuff

--- why_do_i_need_this.mjs
+++ why_do_i_need_this.mjs
@@ -1,16 +1,16 @@
 #!zx
-console.log('SKIPPING');
-// const LOCAL_BUILD_PATH = '.svelte-kit/output/server/';
-// const VERCEL_BUILD_BASE_PATH = '.vercel/output/functions';
-// const vercelFuncDirs = (await $`ls -1d ${VERCEL_BUILD_BASE_PATH}/*.func`).stdout
-//   .split('\n')
-//   .map((v) => v.trim())
-//   .filter((v) => v !== '');
 
-// const outputDirs = [...vercelFuncDirs, LOCAL_BUILD_PATH];
+const LOCAL_BUILD_PATH = '.svelte-kit/output/server/';
+const VERCEL_BUILD_BASE_PATH = '.vercel/output/functions';
+const vercelFuncDirs = (await $`ls -1d ${VERCEL_BUILD_BASE_PATH}/*.func`).stdout
+  .split('\n')
+  .map((v) => v.trim())
+  .filter((v) => v !== '');
 
-// for (const outputDir of outputDirs) {
-//   await $`cp node_modules/@ffmpeg.wasm/core-mt/dist/core.wasm ${outputDir}`;
-//   await $`cp node_modules/@ffmpeg.wasm/core-mt/dist/core.worker.js ${outputDir}/core.worker.cjs`;
-//   await $`cp -r ./src/server/transcripts/audio ${outputDir}`;
-// }
+const outputDirs = [...vercelFuncDirs, LOCAL_BUILD_PATH];
+
+for (const outputDir of outputDirs) {
+  await $`cp node_modules/@ffmpeg.wasm/core-mt/dist/core.wasm ${outputDir}`;
+  await $`cp node_modules/@ffmpeg.wasm/core-mt/dist/core.worker.js ${outputDir}/core.worker.cjs`;
+  await $`cp -r ./src/server/transcripts/audio ${outputDir}`;
+}
