1ff14536cca7f09f2e901aa19df55dbe3c2d9015
Wes Bos <wesbos@gmail.com>
2023-10-27 15:01:11 -0400

    HRM

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import postcssPresetEnv from 'postcss-preset-env';
 import atImport from 'postcss-import';
