5f728cccd7d1636dc9e10dbb9b3170dee734ac64
Wes Bos <wesbos@gmail.com>
2023-10-30 11:01:39 -0400

    Switch to using jpg for opengraph images

--- src/routes/(site)/show/[show_number]/[slug]/[[tab]]/+page.server.ts
+++ src/routes/(site)/show/[show_number]/[slug]/[[tab]]/+page.server.ts
@@ -83,7 +83,7 @@ export const load: PageServerLoad = async function ({ setHeaders, params, locals
 		} as ShowTemp & Show,
 		meta: {
 			title: show_raw?.title,
-			image: `https://${url.host}/og.png?show=${show_number}`,
+			image: `https://${url.host}/og.jpg?show=${show_number}`,
 			description:
 				show_raw?.aiShowNote?.description ?? show_raw?.show_notes?.match(/(.*?)(?=## )/s)?.[0]
 		}
