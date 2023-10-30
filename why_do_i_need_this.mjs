27382d3efe0867dd4dd9db39049e328c09d95579
Wes Bos <wesbos@gmail.com>
2023-10-30 09:50:12 -0400

    vercel

--- why_do_i_need_this.mjs
+++ why_do_i_need_this.mjs
@@ -1,5 +1,5 @@
 #!zx
-
+console.log('Vercel?', process.env.VERCEL);
 const LOCAL_BUILD_PATH = '.svelte-kit/output/server/';
 const VERCEL_BUILD_BASE_PATH = '.vercel/output/functions';
 const vercelFuncDirs = (await $`ls -1d ${VERCEL_BUILD_BASE_PATH}/*.func`).stdout
