5ef154d58cade4c6bbd5950ee94f31a22428cd63
Wes Bos <wesbos@gmail.com>
2023-10-30 12:29:34 -0400

    og image improvements

--- src/lib/meta/Meta.svelte
+++ src/lib/meta/Meta.svelte
@@ -5,16 +5,18 @@
 <svelte:head>
 	<title>{$page.data.meta.title}</title>
 	<meta name="theme-color" content="#000000" />
+	<!-- OG -->
 	<meta property="og:type" content="website" />
-
 	<meta property="og:title" content={$page.data.meta.title} />
-	<meta property="twitter:title" content={$page.data.meta.title} />
-
 	<meta property="og:description" content={$page.data.meta.description} />
 	<meta name="description" content={$page.data.meta.description} />
-
 	<meta property="og:image" content={$page.data.meta.image} />
 	<meta property="og:image:secure_url" content={$page.data.meta.image} />
-	<meta property="twitter:image" content={$page.data.meta.image} />
+	<!-- Twitter -->
+	<meta property="twitter:title" content={$page.data.meta.title} />
 	<meta property="twitter:card" content="summary_large_image" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@syntaxfm" />
+	<meta name="twitter:description" content={$page.data.meta.description} />
+	<meta property="twitter:image" content={$page.data.meta.image} />
 </svelte:head>
