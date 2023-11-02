78ed8fae1255b75173bbf206dfa904acf68eb858
Wes Bos <wesbos@gmail.com>
2023-11-02 17:40:25 -0400

    Newsletter page (#1347)
    * snackpack lander
    
    * remove log

---
+++ src/routes/(site)/snackpack/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import NewsletterForm from '$lib/NewsletterForm.svelte';
+	export let data;
+</script>
+
+<main>
+	<div style:margin-top="2rem">
+		<h1 class="h3 lines">Syntax Snack Pack</h1>
+		<p class="center">Wanna be the <strong>{data.count}</strong> coolest person in the world?</p>
+		<NewsletterForm />
+	</div>
+</main>
