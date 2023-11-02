c9f8de883b97fc5c8866d9a6a1f18d78ec00b910
Scott Tolinski <scott.tolinski@gmail.com>
2023-11-02 13:24:59 -0600

    patch - Layin out catchin some rays (#1340)
    * adds stale while revalidate for shows caching and with variable max age
    
    * removes unneeded deps, cleans
    
    * adds env vars to testing action
    
    * moves env to a global fr action
    
    * adds upslash to env
    
    * adds env to svelte-check action
    
    * updates version bump
    
    * changes node versions for vercel adapter
    
    * tweaks playwright config
    
    * updates playwright server timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,9 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'pnpm build:svelte && pnpm preview',
+		port: 4173,
+		timeout: 600000
 	},
 	testDir: 'tests'
 };
--- src/state/debug.ts
+++
@@ -1,3 +0,0 @@
-import { storable } from './storable';
-
-export const debug_mode = storable(false);
