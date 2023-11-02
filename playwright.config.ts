1447a28ec33fbdbb92400bcaae813c50a87c47c1
Scott Tolinski <scott.tolinski@gmail.com>
2023-11-02 13:09:23 -0600

    updates playwright server timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,8 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'pnpm build:svelte && pnpm preview',
-		port: 4173
+		port: 4173,
+		timeout: 600000
 	},
 	testDir: 'tests'
 };
