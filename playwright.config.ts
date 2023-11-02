527216ebf72aca4f3592c59543d8e63f745b52fa
Scott Tolinski <scott.tolinski@gmail.com>
2023-11-02 13:06:00 -0600

    tweaks playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'pnpm build:svelte && pnpm preview',
 		port: 4173
 	},
 	testDir: 'tests'
