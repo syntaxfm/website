035f8daefd33af025aa4382aba93c4a12a908ccd
Wes Bos <wesbos@gmail.com>
2023-11-27 10:54:24 -0500

    Add prefix to meta tags to fix linked-in OG images

--- src/lib/meta/Meta.svelte
+++ src/lib/meta/Meta.svelte
@@ -6,12 +6,20 @@
 	<title>{$page.data.meta.title}</title>
 	<meta name="theme-color" content="#000000" />
 	<!-- OG -->
-	<meta property="og:type" content="website" />
-	<meta property="og:title" content={$page.data.meta.title} />
-	<meta property="og:description" content={$page.data.meta.description} />
+	<meta prefix="og: http://ogp.me/ns#" property="og:type" content="website" />
+	<meta prefix="og: http://ogp.me/ns#" property="og:title" content={$page.data.meta.title} />
+	<meta
+		prefix="og: http://ogp.me/ns#"
+		property="og:description"
+		content={$page.data.meta.description}
+	/>
 	<meta name="description" content={$page.data.meta.description} />
-	<meta property="og:image" content={$page.data.meta.image} />
-	<meta property="og:image:secure_url" content={$page.data.meta.image} />
+	<meta prefix="og: http://ogp.me/ns#" property="og:image" content={$page.data.meta.image} />
+	<meta
+		prefix="og: http://ogp.me/ns#"
+		property="og:image:secure_url"
+		content={$page.data.meta.image}
+	/>
 	<!-- Twitter -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={$page.data.meta.title} />
