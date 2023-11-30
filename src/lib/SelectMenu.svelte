9df0d0cc508b933a07afc7aab095ffe9459e11c1
Wes Bos <wesbos@gmail.com>
2023-11-30 14:37:47 -0500

    Close popover when filtering item is selected. Fixes #1391

--- src/lib/SelectMenu.svelte
+++ src/lib/SelectMenu.svelte
@@ -21,6 +21,27 @@
 		}
 		return searchParams.toString();
 	};
+
+	// TODO: remove  this when typescirpt dom ships HTMLPopoverElement
+	interface HTMLPopoverElement extends HTMLElement {
+		showPopover: () => void;
+		hidePopover: () => void;
+		togglePopover: () => void;
+		popover: 'auto' | 'manual';
+	}
+	function closePopoverWhenSelected(node: HTMLPopoverElement) {
+		function handlePopoverSelection(event) {
+			if (event.target instanceof HTMLAnchorElement) {
+				node.hidePopover();
+			}
+		}
+		node.addEventListener('click', handlePopoverSelection);
+		return {
+			destroy() {
+				node.removeEventListener('click', handlePopoverSelection);
+			}
+		};
+	}
 </script>
 
 <div style="position: relative;">
@@ -37,7 +58,7 @@
 		{/if}
 		{button_text}
 	</button>
-	<div popover id={popover_id}>
+	<div popover id={popover_id} use:closePopoverWhenSelected>
 		<div class="select-menu-menu-wrapper">
 			{#each options as option}
 				<a
