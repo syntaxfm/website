c2ab40b10367d58dce7fad91ce166847c218157c
Wes Bos <wesbos@gmail.com>
2023-12-07 12:22:27 -0500

    move processor to a util file

---
+++ src/utilities/markdown.ts
@@ -0,0 +1,13 @@
+import remarkGfm from 'remark-gfm';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeRaw from 'rehype-raw';
+import rehypeStringify from 'rehype-stringify';
+
+export const processor = unified()
+	.use(remarkParse)
+	.use(remarkGfm)
+	.use(remarkRehype, { allowDangerousHtml: true })
+	.use(rehypeRaw) // allow html within markdown files
+	.use(rehypeStringify);
