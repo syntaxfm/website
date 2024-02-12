490313c38fd653403d3366b1878832cfe4ad3b61
Scott Tolinski <scott.tolinski@gmail.com>
2024-02-12 13:33:59 -0700

    adds path and tweaks code adds .md to one of the should fail files

--- scripts/merging-show-validation.js
+++ scripts/merging-show-validation.js
@@ -1,6 +1,7 @@
 import { exec } from 'child_process';
 import fs from 'fs/promises';
 import { promisify } from 'util';
+import path from 'path';
 const execAsync = promisify(exec);
 
 // Function to check URL availability
