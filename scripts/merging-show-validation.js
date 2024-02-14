2bd6310f2d7b9f30a37c3cfff9b6d01b82cb396b
Scott Tolinski <scott.tolinski@gmail.com>
2024-02-13 17:33:34 -0700

    tries method head

--- scripts/merging-show-validation.js
+++ scripts/merging-show-validation.js
@@ -12,6 +12,7 @@ async function isUrlValid(url, skipUrls = []) {
 	}
 	try {
 		const response = await fetch(url, {
+			method: 'HEAD',
 			headers: {
 				'User-Agent':
 					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
