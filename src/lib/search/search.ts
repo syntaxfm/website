f9d2caf58e23d7260d22e25d78828b9de6045017
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-21 08:58:21 -0600

    feat: strip markdown links in search (#1564)
    * feat: strip markdown links in search
    
    * chore: feedback

--- src/lib/search/search.ts
+++ src/lib/search/search.ts
@@ -63,7 +63,14 @@ export function search(query: string) {
 				(a?.block?.breadcrumbs.length || 0) - (b?.block?.breadcrumbs.length || 0) || a.rank - b.rank
 			);
 		})
-		.map(({ block }) => block) as (Block & Show)[];
+		.map(({ block }) => {
+			return {
+				...block,
+				content: block?.content
+					// strip markdown braces but preserve the link text as well as the link, delimited by a hyphen
+					.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '$1 - $2')
+			};
+		}) as (Block & Show)[];
 
 	const results = tree([], blocks).children;
 
