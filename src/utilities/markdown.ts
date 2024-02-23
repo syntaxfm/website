af6fd1ce07f5b03b00d5fcbe707e77e8c3925bb8
Wes Bos <wesbos@gmail.com>
2024-02-23 13:12:11 -0500

    Adds links and IDs to markdown headings (#1578)
    * Add heading IDs
    
    * Dependency
    
    * refactor markdown processing, add IDs and links to headers
    
    * add types for untyped libraru
    
    ---------
    
    Co-authored-by: Thomas Steiner <tomac@google.com>

--- src/utilities/markdown.ts
+++ src/utilities/markdown.ts
@@ -3,11 +3,17 @@ import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeRaw from 'rehype-raw';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeStringify from 'rehype-stringify';
+import rehypeHighlight from 'rehype-highlight';
+import remarkHeadingId from 'remark-heading-id';
 
 export const processor = unified()
-	.use(remarkParse)
+  .use(remarkParse)
 	.use(remarkGfm)
+  .use(remarkHeadingId, { defaults: true })
 	.use(remarkRehype, { allowDangerousHtml: true })
 	.use(rehypeRaw) // allow html within markdown files
-	.use(rehypeStringify);
+  .use(rehypeHighlight)
+	.use(rehypeStringify)
+  .use(rehypeAutolinkHeadings, { behavior: 'append', })
