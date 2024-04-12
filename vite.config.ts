7605ede070af309b1636e4863038fe7da7649a49
Scott Tolinski <scott.tolinski@gmail.com>
2024-04-12 10:45:01 -0600

    adds codecov  bundle eval on build

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,37 @@
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { codecovVitePlugin } from '@codecov/vite-plugin';
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'syntax-fm',
-				project: 'website',
-				authToken: process.env.SENTRY_AUTH_TOKEN,
-				cleanArtifacts: true
-			}
-		}),
-		sveltekit()
-	],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	css: {
-		devSourcemap: true,
-		preprocessorOptions: {
-			postcss: {
-				additionalData: `
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [
+			sentrySvelteKit({
+				sourceMapsUploadOptions: {
+					org: 'syntax-fm',
+					project: 'website',
+					authToken: env.SENTRY_AUTH_TOKEN,
+					cleanArtifacts: true
+				}
+			}),
+			sveltekit(),
+			codecovVitePlugin({
+				enableBundleAnalysis: env.CODECOV_TOKEN !== undefined,
+				bundleName: 'website',
+				uploadToken: env.CODECOV_TOKEN
+			})
+		],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		},
+		css: {
+			devSourcemap: true,
+			preprocessorOptions: {
+				postcss: {
+					additionalData: `
 				@custom-media --below_small (width < 400px);
 				@custom-media --below_med (width < 700px);
 				@custom-media --below_large (width < 900px);
@@ -33,21 +43,22 @@ export default defineConfig({
 				@custom-media --above_large (width > 900px);
 				@custom-media --above_xlarge (width > 1200px);
 				`
+				}
 			}
+		},
+		ssr: {
+			external: ['@sentry/profiling-node']
+		},
+		esbuild: {
+			exclude: '@sentry/profiling-node'
+		},
+		resolve: {
+			alias: {
+				'.prisma/client/index-browser': './node_modules/@prisma/client/index-browser.js'
+			}
+		},
+		define: {
+			__VER__: JSON.stringify(env.npm_package_version)
 		}
-	},
-	ssr: {
-		external: ['@sentry/profiling-node']
-	},
-	esbuild: {
-		exclude: '@sentry/profiling-node'
-	},
-	resolve: {
-		alias: {
-			'.prisma/client/index-browser': './node_modules/@prisma/client/index-browser.js'
-		}
-	},
-	define: {
-		__VER__: JSON.stringify(process.env.npm_package_version)
-	}
+	};
 });
