a22505966d448dc4e627712cb61eb2dd0d54d432
msmps <7691252+msmps@users.noreply.github.com>
2024-04-28 21:48:33 +0100

    feat: add spotify urls to database

--- src/server/shows.ts
+++ src/server/shows.ts
@@ -12,6 +12,7 @@ interface FrontMatterGuest {
 	twitter: string;
 	url: string;
 	youtube_url?: string;
+	spotify_url?: string;
 	social: string[];
 }
 
@@ -54,7 +55,7 @@ export async function import_or_update_all_changed_shows() {
 
 			// If show doesn't exist or the hash has changed. Refresh
 			if (!existing_show || existing_show.hash !== hash) {
-        console.log(`Refreshing Show # ${number}`);
+				console.log(`Refreshing Show # ${number}`);
 				await parse_and_save_show_notes(md_file_contents, hash, number, md_file_path);
 			}
 		}
@@ -99,8 +100,9 @@ export async function parse_and_save_show_notes(
 				slug: slug(data.title),
 				date,
 				number,
-        url: data.url,
+				url: data.url,
 				youtube_url: data.youtube_url,
+				spotify_url: data.spotify_url,
 				show_notes: content,
 				hash: hash,
 				md_file,
@@ -114,6 +116,7 @@ export async function parse_and_save_show_notes(
 				date,
 				url: data.url,
 				youtube_url: data.youtube_url,
+				spotify_url: data.spotify_url,
 				show_notes: content,
 				hash: hash,
 				md_file,
