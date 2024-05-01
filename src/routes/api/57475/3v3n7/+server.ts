2c95210990d5259fbd4216dfc1b81ecb3d0738d7
Wes Bos <wesbos@gmail.com>
2024-05-01 16:16:54 -0400

    proxy work

--- src/routes/api/57475/3v3n7/+server.ts
+++ src/routes/api/57475/3v3n7/+server.ts
@@ -6,6 +6,29 @@ export const OPTIONS = optionsHandler();
 
 export const POST: RequestHandler = async ({ request, locals }) => {
 	// 1. Make a copy of the headers so we can modify them. This is necessary because the headers are immutable.
+	// const headers = new Headers(request.headers);
+	const req = new Request(request);
+	// 2. Delete any cookies - we dont need them
+	req.headers.delete('cookie');
+	// 3. Set the IP address of the client. Because we use cloudflare, we need to get the IP address from the headers
+	const ip =
+		request.headers.get('true-client-ip') || // Cloudflare
+		request.headers.get('x-forwarded-for') || // Common Proxy / Load Balancer (Vercel included)
+		locals.session.ip; // Svelte backup, probably just x-forwarded-for
+	// Logging for testing, delete for production
+	console.log('IP:', ip, {
+		trueClientIp: request.headers.get('true-client-ip'),
+		xForwardedFor: request.headers.get('x-forwarded-for')
+	});
+	// const headers = req.headers;
+	req.headers.set('X-Forwarded-For', ip);
+	// Recreate the request with the new headers, we have to do it this way because the headers are immutable
+	const res = await fetch('https://plausible.io/api/event', req);
+	return new Response(res.body, res);
+};
+
+const POSTOLD: RequestHandler = async ({ request, locals }) => {
+	// 1. Make a copy of the headers so we can modify them. This is necessary because the headers are immutable.
 	const headers = new Headers(request.headers);
 	// 2. Delete any cookies - we dont need them
 	headers.delete('cookie');
@@ -20,7 +43,7 @@ export const POST: RequestHandler = async ({ request, locals }) => {
 		xForwardedFor: request.headers.get('x-forwarded-for')
 	});
 	// const headers = req.headers;
-	headers.set('X-Forwarded-For', ip);
+	// headers.set('X-Forwarded-For', ip);
 	// Recreate the request with the new headers, we have to do it this way because the headers are immutable
 	return fetch('https://plausible.io/api/event', {
 		body: request.body,
@@ -38,39 +61,39 @@ export const POST: RequestHandler = async ({ request, locals }) => {
 		signal: request.signal
 	});
 
-	const body = await request.text();
-	const response = await fetch('https://plausible.io/api/event', {
-		method: 'POST',
-		body,
-		headers: {
-			'User-Agent': request.headers.get('user-agent') || 'unknown',
-			'Content-Type': 'application/json',
-			'X-Forwarded-For': locals.session.ip
-		}
-	});
-	const responseBody = await response.text();
-	if (!response.ok) {
-		const message = `Error proxying plausible event: ${JSON.stringify(
-			{
-				status: response.status,
-				statusText: response.statusText,
-				headers: [...response.headers.entries()],
-				body: responseBody
-			},
-			null,
-			2
-		)}`;
-		console.error(message);
-		const errorId = crypto.randomUUID();
-		Sentry.captureException(new Error(message), {
-			extra: { event, errorId, status: response.status }
-		});
-	}
-	return new Response(responseBody, {
-		status: response.status,
-		statusText: response.statusText,
-		headers: {
-			'Content-Type': response.headers.get('content-type') || 'text/plain'
-		}
-	});
+	// const body = await request.text();
+	// const response = await fetch('https://plausible.io/api/event', {
+	// 	method: 'POST',
+	// 	body,
+	// 	headers: {
+	// 		'User-Agent': request.headers.get('user-agent') || 'unknown',
+	// 		'Content-Type': 'application/json',
+	// 		'X-Forwarded-For': locals.session.ip
+	// 	}
+	// });
+	// const responseBody = await response.text();
+	// if (!response.ok) {
+	// 	const message = `Error proxying plausible event: ${JSON.stringify(
+	// 		{
+	// 			status: response.status,
+	// 			statusText: response.statusText,
+	// 			headers: [...response.headers.entries()],
+	// 			body: responseBody
+	// 		},
+	// 		null,
+	// 		2
+	// 	)}`;
+	// 	console.error(message);
+	// 	const errorId = crypto.randomUUID();
+	// 	Sentry.captureException(new Error(message), {
+	// 		extra: { event, errorId, status: response.status }
+	// 	});
+	// }
+	// return new Response(responseBody, {
+	// 	status: response.status,
+	// 	statusText: response.statusText,
+	// 	headers: {
+	// 		'Content-Type': response.headers.get('content-type') || 'text/plain'
+	// 	}
+	// });
 };
