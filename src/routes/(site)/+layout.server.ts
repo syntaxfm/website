aad3a1bf6b3c41fd3a9f3d6791a0adad9e849c6d
Scott Tolinski <scott.tolinski@gmail.com>
2024-07-18 11:33:02 -0600

    removes unessary todo

--- src/routes/(site)/+layout.server.ts
+++ src/routes/(site)/+layout.server.ts
@@ -1,8 +1,5 @@
 import { cache } from '$/server/cache/cache';
 export const load = async ({ locals }) => {
-	const cache_s = 600;
-	// TODO add s override
-
 	return {
 		// We load latest in layout to get the most recent track in player data
 		// auto gets passed into that page
