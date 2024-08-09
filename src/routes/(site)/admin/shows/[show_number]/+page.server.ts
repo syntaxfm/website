bb044060aaf5195edbe374993f201772c694a7b0
CJ <cj@null.computer>
2024-08-09 09:40:44 -0600

    feat: allow edit of AI generated title and description (#1773)

--- src/routes/(site)/admin/shows/[show_number]/+page.server.ts
+++ src/routes/(site)/admin/shows/[show_number]/+page.server.ts
@@ -1,4 +1,37 @@
+import type { AiShowNote } from '@prisma/client';
 import type { PageServerLoad } from './$types';
+import type { Actions } from './$types';
+import { fail } from '@sveltejs/kit';
+import { cache } from '$/server/cache/cache';
+
+type AiShowNoteUpdate = Pick<AiShowNote, 'title' | 'description'>;
+
+export const actions = {
+	default: async ({ locals, params }) => {
+		const data = locals.form_data as AiShowNoteUpdate;
+		if (!data.title.trim()) {
+			return fail(400, { message: 'AI title cannot be empty.' });
+		}
+		if (!data.description.trim()) {
+			return fail(400, { message: 'AI description cannot be empty.' });
+		}
+
+		const show_number = parseInt(params.show_number);
+		await locals.prisma.aiShowNote.update({
+			where: {
+				show_number
+			},
+			data: {
+				title: data.title,
+				description: data.description
+			}
+		});
+
+		await cache.shows.drop_show_cache(show_number);
+
+		return { success: true };
+	}
+} satisfies Actions;
 
 export const load: PageServerLoad = async ({ locals, params }) => {
 	return {
--- src/routes/(site)/admin/shows/[show_number]/+page.svelte
+++ src/routes/(site)/admin/shows/[show_number]/+page.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import type { PageData, ActionData } from './$types';
 	import Dump from './Dump.svelte';
 	export let data: PageData;
+	export let form: ActionData;
+	let formRef: HTMLFormElement | null = null;
 	$: ({ show } = data);
+
+	$: if (form) {
+		formRef?.scrollIntoView({
+			behavior: 'smooth',
+		});
+	}
 </script>
 
 <h1 class="h4">DB dump</h1>
@@ -11,9 +19,32 @@
 	<Dump data={show} />
 {/if}
 <h1 class="h4">AI Show Notes</h1>
-
 {#if show?.aiShowNote}
+	<form class="flex flex-col" method="POST" bind:this={formRef}>
+		{#if form?.message}
+			<div class="errors">
+				{form?.message}
+			</div>
+		{/if}
+		<label for="title">Title</label>
+		<input name="title" id="title" value={show?.aiShowNote.title} />
+		<label for="description">Description</label>
+		<textarea name="description" id="description" rows={4} value={show?.aiShowNote.description}></textarea>
+		<button>Update</button>
+	</form>
 	<Dump data={show?.aiShowNote} />
 {:else}
 	<p>Notes not available</p>
 {/if}
+
+<style>
+	form {
+		margin: 1rem 0;
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+	}
+	.errors {
+		color: red;
+	}
+</style>
