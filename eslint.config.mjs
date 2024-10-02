8b13d4b972b5384300a1283af1906bf95754ef22
Scott Tolinski <scott.tolinski@gmail.com>
2024-10-02 14:16:56 -0600

    moves ignore largely to .gitignore and brings in @eslint/compat for ignore share

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,21 @@
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
 import svelteParser from 'svelte-eslint-parser';
 import js from '@eslint/js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
 export default [
+	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	{
-		ignores: [
-			'**/node_modules/**',
-			'build/**',
-			'.svelte-kit/**',
-			'package/**',
-			'**/*.d.ts',
-			'.vercel/**'
-		]
+		ignores: ['**/*.d.ts']
 	},
 	{
 		files: ['**/*.{js,ts,svelte}'],
