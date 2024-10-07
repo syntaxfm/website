5390061d2ba3dc8fb49ca095711c1f2aec28de61
Wes Bos <wesbos@gmail.com>
2024-10-07 11:19:21 -0400

    Page description

--- src/routes/(site)/oss/+page.server.ts
+++ src/routes/(site)/oss/+page.server.ts
@@ -4,7 +4,8 @@ import { UserSubmissionAction } from '$/lib/forms/UserSubmissionAction';
 export const load: PageServerLoad = async function () {
 	return {
 		meta: {
-			title: '$50,000 for Open Source'
+			title: '$50,000 for Open Source',
+			description: 'Syntax is Pledging $50,000 to open source projects.'
 		}
 	};
 };
