8d46c6473e1c9ea42f0a52e44ed3ef3bf60d09e8
Wes Bos <wesbos@gmail.com>
2024-10-21 11:44:16 -0400

    fix wrong error import

--- src/routes/webhooks/refresh/+server.ts
+++ src/routes/webhooks/refresh/+server.ts
@@ -1,9 +1,7 @@
 import { import_or_update_all_changed_shows } from '../../../server/shows';
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { has_auth } from '../transcripts/has_auth';
-import { error } from 'console';
-
 export const GET: RequestHandler = async function ({ request }) {
 	const allowed = has_auth(request);
 	if (!allowed) {
