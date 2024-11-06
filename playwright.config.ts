3faac255fca594fc42872d89e6fe244fd66e45d1
Scott Tolinski <scott.tolinski@gmail.com>
2024-11-06 18:35:36 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,20 @@
-const config = {
-	webServer: {
-		command: 'pnpm build:svelte && pnpm preview',
-		port: 4173,
-		timeout: 600000
-	},
-	testDir: 'tests'
-};
+import { defineConfig, devices } from '@playwright/test';
+
+const config = defineConfig({
+  fullyParallel: true,
+  retries: process.env.CI ? 2 : 0,
+  webServer: {
+    command: 'pnpm build:svelte && pnpm preview',
+    port: 4173,
+    timeout: 600_000
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  testDir: 'tests'
+});
 
 export default config;
