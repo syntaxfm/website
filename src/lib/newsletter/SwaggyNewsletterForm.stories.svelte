f1b46c2a01420dc55acbf5caea4dfed3fedd23d3
Scott Tolinski <scott.tolinski@gmail.com>
2025-04-10 11:08:41 -0600

    starts stories

---
+++ src/lib/newsletter/SwaggyNewsletterForm.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import SwaggyNewsletterForm from './SwaggyNewsletterForm.svelte';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Content/SwaggyNewsletterForm',
+		component: SwaggyNewsletterForm
+	});
+</script>
+
+<Story name="Default" />
